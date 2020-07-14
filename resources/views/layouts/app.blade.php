6736fe533d9def827e5cabb7bb3edb6a5ebd0266
marcoinsabato <tu@esempio.com>
2020-07-14 12:24:49 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,13 +31,8 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto font-weight-bold">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
